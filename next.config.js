002ed8a75a8677918067bc5c67b939b111bb39be
Jacob <39503993+novosta@users.noreply.github.com>
2024-05-30 23:51:25 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
+  assetPrefix: 'https://dl.novosta.bio',
+  images: {
+    domains: ['cdn.novosta.bio'],
+  },
   i18n,
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
