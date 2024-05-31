f2fea395eb90707b8e64e82e5911e62ba6c439b3
Jacob <39503993+novosta@users.noreply.github.com>
2024-05-30 23:49:45 -0700

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -1,14 +1,19 @@
 module.exports = {
-  title: 'Your Custom CDN',
-  description: 'Description of your CDN',
+  clientId: '',
+  obfuscatedClientSecret: '',
+  redirectUri: 'http://localhost',
+  authApi: '',
+  driveApi: '',
+  scope: '',
+  cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
+  email: '',
   googleFontSans: 'Inter',
   googleFontMono: 'Fira Mono',
   googleFontLinks: [
     'https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap',
   ],
-  footer: 'Powered by your custom CDN. Made with ❤ by your name.',
+  footer: 'Powered by your custom CDN. Made with ❤ by Jacob.',
   protectedRoutes: [],
-  email: '',
   links: [],
-  cdnBaseUrl: 'https://cdn.novosta.bio', // Add this line
+  datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
 };
